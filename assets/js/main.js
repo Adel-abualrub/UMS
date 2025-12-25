40895f78e479fe75b693d321c033280a360b562a
Adel-abualrub <adelbassam5201@gmail.com>
2025-12-25 12:22:27 +0200

    Delete users

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const GetData= async ()=>{
-const data= await axios.get('http://ums12.runasp.net/api/users?limit=200')
+const data= await axios.get(`http://ums12.runasp.net/api/users?limit=200`)
 
 return data.data;
 
@@ -15,10 +15,20 @@ return `
 <td> ${user.email} </td>
 <td> ${user.age} </td>
 <td> <img src="${user.imageUrl}" alt="User Img" width=100px height=50px> </td>
+<td> <button class="btn btn-danger " onclick=DeleteUser(${user.id})>Delete</button></td>
 
 `})
 document.querySelector('.Users-data').innerHTML=users.join(" ");
 
 }
+const DeleteUser=async (id)=>{
+const DLuser= await axios.delete(`http://ums12.runasp.net/api/users/${id}`)
+
+
+
+
+}
+
+
 displayData();
 
